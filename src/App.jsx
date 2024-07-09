da07e8c3a6fb2eacd679614d2b90e6dc3aed9e9f
karpagakirthika <velusamyg2004@gmail.com>
2024-07-09 15:43:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { FaRegEye } from "react-icons/fa6";
+import HeaderComponent from './assets/Components/HeaderComponent';
+function App() {
+  
+
+  return (
+    <>
+    <div className='box'><FaRegEye  className='icon'/>
+    <HeaderComponent name="KARPAGAKIRTHIKA" regno="212221220025"/>
+    </div>
+   
+
+
+    </>
+  )
+}
+
+export default App
